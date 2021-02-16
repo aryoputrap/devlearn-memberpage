4bf42f981722ac58e7e97f09fd217074fd5e07e3
Aryo Putra <aryoputrap@gmail.com>
2021-02-16 08:40:37 +0700

    test

--- src/pages/Joined.js
+++ src/pages/Joined.js
@@ -12,19 +12,6 @@ export default function Joined({ history, match }) {
     data: {},
   }));
 
-  // const joining = React.useCallback(async () => {
-  //   try {
-  //     const details = await courses.details(match.params.class);
-
-  //     const joined = await courses.join(match.params.class);
-  //     if (joined.data.snap_url) window.location.href = joined.data.snap_url;
-  //     else setstate({ isLoading: false, isError: false, data: details });
-  //   } catch (error) {
-  //     if (error?.response?.data?.message === "user already take this course")
-  //       history.push(`/courses/${match.params.class}`);
-  //   }
-  // }, [match.params.class]);
-
   useEffect(() => {
     courses
     .details(match.params.class)
