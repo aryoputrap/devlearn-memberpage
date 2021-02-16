f927c89f29169e022ff2c2db7073f7ad6fbf9ae7
Aryo Putra Purwanto <42566360+aryoputrap@users.noreply.github.com>
2021-02-16 08:57:07 +0700

    Update Sidebar.js

--- src/parts/Sidebar.js
+++ src/parts/Sidebar.js
@@ -59,7 +59,7 @@ function Sidebar({ match, history }) {
                   <img
                     className="object-cover w-24 h-24"
                     src={USERS?.avatar}
-                    // alt={USERS?.name}
+                    alt={USERS?.name}
                   />
                 ) : (
                   <DefaultUser className="fill-indigo-500 w-24 h-24"></DefaultUser>
